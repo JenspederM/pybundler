62e3cbd3b506ec8363e297f7fd41fff0a625f0b1
Jens Peder Meldgaard <jenspederm@gmail.com>
2025-03-30 18:30:19 +0200

    add fastapi example
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

---
+++ examples/fastapi-app/src/fastapi_app/main.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+def serve():
+    uvicorn.run("fastapi_app.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    serve()
